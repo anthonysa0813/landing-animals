7108fd3f13e01b508fa3ba20b116536411ca8387
anthony sanchez <anthonysa0813@gmail.com>
2021-09-17 13:26:37 -0500

    useContext in the skills area and form aerea

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UsuarioContext } from "../../context/UsuarioContext";
 import logo from "../../images/logo-one.png";
+
 import menu from "../../images/menu.svg";
 import notification from "../../images/notification.svg";
 import "./header.css";
 
 const Header = () => {
+  const { usuario } = useContext(UsuarioContext);
+  const { nombre, role, level } = usuario;
   return (
     <header>
       <div className="header-logo  "></div>
@@ -19,7 +23,7 @@ const Header = () => {
             alt="logo de notificación"
             className="notification"
           />
-          <p>Cesar Avellaneda</p>
+          <p>{nombre ? nombre : "Cesar Avellaneda"}</p>
         </div>
       </div>
     </header>
